8175bba86db7ddab8e79bd7a68552188f09c8b12
Jackweb-rgba <radohery09@gmail.com>
2024-08-19 19:40:07 +0200

    mije à jour

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-
 const baliseNavLinks = document.getElementById("nav_links");
 const balisefooter = document.getElementById("footer");
 const balisehamburger = document.getElementById("hamburger");
@@ -7,7 +6,14 @@ const baliseImage = document.getElementById("image");
 const baliseMenus = document.querySelectorAll("div.navbar_menu a");
 const baliseHeader = document.querySelector("header");
 const baliseBody = document.querySelector("body");
-console.log(baliseHeader);
+const progressBar = document.getElementById("progressBar");
+const containerCard = document.getElementById('containerCard');
+const allCard = document.querySelectorAll('.projetView');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const tableTr = document.querySelectorAll("#tr");
+const skillsProgres = document.querySelectorAll('.progress');
+
 // hamburger
 let close = true; 
 balisehamburger.addEventListener("click",(e) =>{
@@ -32,91 +38,108 @@ function removeUnderlineMenus(baliseMenus){
         }
     }
 }
+function removeActive(baliseMenus){
+    for (let i=0;i<5;i++){
+        if (baliseMenus[i].classList[1] == "active"){
+            baliseMenus[i].classList.remove("active");
+            break;
+        }
+    }
+}
 // deplacer underline
 for (let i=0;i<5;i++){
     baliseMenus[i].addEventListener("click", ()=>{
         removeUnderlineMenus(baliseMenus);
+        removeActive(baliseMenus);
         baliseMenus[i].classList.add("underline")
+        baliseMenus[i].classList.add("active");
     }) 
 }
-// petit anime baliseProfile
-a =54; b=39; c=18; d=76; e=50; f=63; g=30; h=48; bttm = -30;
-toureA = true; toureB = true; toureC = true; toureD = true; toureE = true; toureF = true; toureG = true; toureH = true; toureBttm = true;
-let interval = setInterval(() =>{
-    baliseProfile.style.borderRadius = a+"% "+b+"% "+c+"% "+d+"% / "+e+"% "+f+"% "+g+"% "+h+"%"; // 54% 39% 18% 76% / 50% 63% 30% 48%
-    let x = 46;
-    if (toureA){ a++; if (a == 54+x){ toureA = false;}}else{if (a == 46){toureA = true;}a--; }
-    if (toureB){ b++; if (b == 39+x){ toureB = false;}}else{if (b == 39){toureB = true;}b--; }
-    if (toureC){ c++; if (c == 18+x){ toureC = false;}}else{if (c == 18){toureC = true;}c--; }
-    if (toureD){ d++; if (d == 76+x){ toureD = false;}}else{if (d == 76){toureD = true;}d--; }
-    if (toureE){ e++; if (e == 50+x){ toureE = false;}}else{if (e == 42){toureE = true;}e--; }
-    if (toureF){ f++; if (f == 63+x){ toureF = false;}}else{if (f == 63){toureF = true;}f--; }
-    if (toureG){ g++; if (g == 30+x){ toureG = false;}}else{if (g == 30){toureG = true;}g--; }
-    if (toureH){ h++; if (h == 48+x){ toureH = false;}}else{if (h == 48){toureH = true;}h--; }
-
-    baliseImage.style.bottom = bttm + 'px';
-    if (toureBttm){
-        bttm = bttm + 1;
-        if (bttm == -10){
-            toureBttm = false;
-        }
-    }else{
-        bttm = bttm - 1;
-        if (bttm == -30){
-            toureBttm = true;
-        }
-    }
-
-},100);
-
-/*bttm = -30; left = 0;
-toureBttm = true; toureLeft = true;
-let interval2 = setInterval(() => {
-    baliseImage.style.left = left + 'px';
-    baliseImage.style.bottom = bttm + 'px';
-    if (toureBttm){
-        bttm = bttm + 0.1;
-        // left++; 
-        if (left == 5){
-            toureLeft = false
-        }
-        if (bttm == -25){
-            toureBttm = false;
-        }
-    }else{
-        bttm = bttm - 0.1;
-        // left--;
-        if (left == 0){
-            toureLeft = true;
-        }
-        if (bttm == -30){
-            toureBttm = true;
-        }
-    }
-    console.log(' bttm :'+bttm);
-}, 10);*/
-
-// ripplie on click
+// ripplie on click profile
 baliseProfile.addEventListener('click', (e) =>{
     const x = e.clientX - e.target.offsetLeft;
-    const y = e.clientY - e.target.offsetTop;
-    console.log("x :"+x+" y:"+y);
-
+    const y = e.clientY - e.target.offsetTop + window.scrollY;
     const ripplie = document.createElement('span');
+    console.log(window.scrollY);
+
     ripplie.classList.add('span');
     ripplie.style.left = x + 'px';
     ripplie.style.top = y + 'px';
     baliseProfile.appendChild(ripplie);
-
     setTimeout(() => {
         ripplie.remove();
     }, 1000);
 })
 
+// ripplie on click table
+tableTr.forEach(tr => {
+    tr.addEventListener('click', (e) =>{
+        Notification.requestPermission()
+            .then(premission => {
+                if (premission == 'granted') {
+                    new Notification("Bonjour, vous pouvez télécharger mon CV , pour avoir plus d'information.");
+                }
+            })
+    
+    })
+});
+
 // on scrolln => sticky for header
 window.addEventListener("scroll",(e) =>{    
     baliseHeader.classList.toggle("sticky",window.scrollY > 0);
     baliseHeader.classList.toggle("sticky2",window.scrollY > 99);
-    console.log("X :"+window.scrollY);
 })
 
+//progress Bar 
+let scroll = document.body.scrollHeight - window.innerHeight; 
+window.addEventListener("scroll",(e) =>{
+    let Y = window.scrollY;
+    let H = (Y * 100) / scroll;
+    progressBar.style.height = H + 'vh';
+});
+
+// scorll slide project view
+// default scroll
+let x = allCard[0].offsetWidth * allCard.length;
+let x_scroll = x / 3;
+containerCard.scrollTo({left:x_scroll});
+// scroll to left
+prevBtn.addEventListener('click', (e) =>{
+    if (x_scroll > 0){
+        x_scroll = x_scroll - allCard[0].offsetWidth;
+    }
+    containerCard.scrollTo({left:x_scroll,behavior:'smooth'});
+})
+// scroll to right
+nextBtn.addEventListener('click', (e) =>{
+    if (x_scroll < (x-(allCard[0].offsetWidth*3))){
+        x_scroll = x_scroll + allCard[0].offsetWidth;
+    }
+    containerCard.scrollTo({left:x_scroll,behavior:'smooth'}); 
+})
+
+// les progressBar Skills
+const widthP = new Array(80,70,40,15,30,15,5,30);
+const max = document.getElementById("skillsProgres0").offsetWidth;
+for (let i = 0;i<skillsProgres.length;i++){
+    const progressBarSkill = document.getElementById("progressBarSkill"+i);
+    progressBarSkill.style.display = "none";
+
+    const container = document.getElementById("skillsProgres"+i);
+    const Element = document.getElementById("skillsImg"+i);
+    const pourcentage = document.getElementById("pourcentage"+i);
+    container.addEventListener('mouseover',(e) =>{
+        Element.style.display = "none";
+        container.style.justifyContent = "left";
+        progressBarSkill.style.display = "flex";
+        progressBarSkill.style.width = ((widthP[i] * max) / 100)  + 'px';
+
+        // animateWidth();
+    })
+    container.addEventListener('mouseout', (e) =>{
+        progressBarSkill.style.display = "none";
+        container.style.justifyContent = "center";
+        Element.style.display = "flex";
+        progressBarSkill.style.width = 0 +'px';
+    })
+}
